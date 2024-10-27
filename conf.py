3f7e9ee0c6183a76fa36be058bb967bd448339ec
Václav Šmilauer <eu@doxos.eu>
2024-10-27 21:13:39 +0100

    move to sphinx-book theme

--- conf.py
+++ conf.py
@@ -43,7 +43,6 @@ except subprocess.CalledProcessError:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinxcontrib.fulltoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -79,16 +78,26 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    # 'fixed_sidebar':'true',
-    # 'sidebar_collapse':'true',
-}
+html_theme_options = dict(
+  #show_nav_level=4,
+  #navigation_depth=4,
+  #show_navbar_depth=10,
+  #max_navbar_depth=10,
+  #navigation_with_keys=False,
+  use_download_button=False,
+  use_source_button=False,
+  repository_provider='github',
+  repository_url='https://github.com/edhamma/cfitness',
+  use_edit_page_button=True,
+  use_repository_button=True,
+  use_issues_button=True,
+)
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
